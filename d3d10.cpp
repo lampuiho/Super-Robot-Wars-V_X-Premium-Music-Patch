049e0434bf2906111c77cd02ca52ae9d2b97178f
Howard Lam <puiholam2-c@my.cityu.edu.hk>
2024-12-22 12:02:23 +0000

    remove avx related

--- d3d10.cpp
+++ d3d10.cpp
@@ -17,59 +17,6 @@ FARPROC p[3] = {0};
 const BYTE CODE[] = { 0xB0,0x01,0x88,0x01,0xEB,0x18,0x0F,0x1F,0x00 };
 const BYTE AOB[] = { 0x48,0x83,0xEC,0x30,0x48,0x8B,0xF9,0x48,0x8D,0x0D,0,0,0,0,0xE8,0,0,0,0,0x3C,0x01 };
 const UINT32 BitMasks[] = { ~(UINT32)0b110000100001111111111 };
-
-__m256i getXMMask(const UINT32 mask) {
-  __m256i vmask(_mm256_set1_epi32(mask));
-  const __m256i shuffle(_mm256_setr_epi64x(0x0000000000000000,
-      0x0101010101010101, 0x0202020202020202, 0x0303030303030303));
-  vmask = _mm256_shuffle_epi8(vmask, shuffle);
-  const __m256i bit_mask(_mm256_set1_epi64x(0x7fbfdfeff7fbfdfe));
-  vmask = _mm256_or_si256(vmask, bit_mask);
-  return _mm256_cmpeq_epi8(vmask, _mm256_set1_epi64x(-1));
-}
-
-UINT64 __fastcall aobScanRegionAVX2(const BYTE* aob, size_t len, const UINT32* masks, const BYTE* start, size_t sizeOfRegion){
-	auto end = start + sizeOfRegion - len;
-	auto mIter = start;
-	auto aobEnd = aob+len;
-	//need to mask invalid bit
-	__m256i vAob = _mm256_lddqu_si256((__m256i const *)aob);
-	__m256i mask = getXMMask(*masks);
-	while (mIter < end){
-		__m256i vDat = _mm256_lddqu_si256((__m256i const *)mIter);
-		__m256i out = _mm256_cmpeq_epi8(vAob,vDat);
-		out = _mm256_or_si256(out,mask);
-		if (~_mm256_movemask_epi8(out) == 0){
-			auto mIterInner = mIter + 32;
-			auto aobIter = aob + 32;
-			auto maskIter = masks+1;
-			
-			while (aobIter < aobEnd){
-				vAob = _mm256_lddqu_si256((__m256i const *)aobIter);
-				mask = getXMMask(*maskIter);
-				vDat = _mm256_lddqu_si256((__m256i const *)mIter);
-				__m256i out = _mm256_cmpeq_epi8(vAob,vDat);
-				out = _mm256_or_si256(out,mask);
-				
-				if (~_mm256_movemask_epi8(out) == 0){
-					mIterInner += 32;
-					aobIter += 32;
-					maskIter++;
-				}
-				else{
-					vAob = _mm256_lddqu_si256((__m256i const *)aob);
-					mask = getXMMask(*masks);
-					break;
-				}
-			}
-			if (aobIter >= aobEnd) //found match for first set of bytes
-				return (UINT64)mIter;
-		}
-		mIter++; //assume single byte align
-	}
-	return 0;
-}
-
 // this function converts 8bits to 8bytes... slowly...
 UINT64 getMask(const UINT32* masks, size_t bitPos){
 	BYTE out[8];
@@ -84,7 +31,6 @@ UINT64 getMask(const UINT32* masks, size_t bitPos){
 	}
 	return *(UINT64*)out;
 }
-
 //assumed we don't run to the end of the region...
 UINT64 __fastcall aobScanRegionSlow(const BYTE* aob, size_t len, const UINT32* masks, const BYTE* start, size_t sizeOfRegion){
 	auto end = start + sizeOfRegion - len;
@@ -124,14 +70,6 @@ UINT64 __fastcall aobScanRegionSlow(const BYTE* aob, size_t len, const UINT32* m
 	}
 	return 0;
 }
-
-UINT64 aobScanRegion(const BYTE* aob, size_t len, const UINT32* masks, const BYTE* start, size_t sizeOfRegion){
-	if(__builtin_cpu_supports("avx2"))
-		return aobScanRegionAVX2(aob, len, masks, start, sizeOfRegion);
-	else
-		return aobScanRegionSlow(aob, len, masks, start, sizeOfRegion);
-}
-
 // returns address of first aob found. 0 if not found.
 UINT64 aobScanProcess(const BYTE* aob, size_t len, const UINT32* masks){
 	MODULEINFO info;
@@ -139,14 +77,10 @@ UINT64 aobScanProcess(const BYTE* aob, size_t len, const UINT32* masks){
 	//get base of process memory region and its size
 	HMODULE base = GetModuleHandle(NULL);
 	bool foundInfo = GetModuleInformation(GetCurrentProcess(),base,&info,sizeof(info));
-
-	if(foundInfo)
-		return aobScanRegion(aob, len, masks, (BYTE*)base, info.SizeOfImage);
-	//	return aobScanRegionSlow(aob, len, masks, (BYTE*)base, info.SizeOfImage);
+	if(foundInfo) return aobScanRegionSlow(aob, len, masks, (BYTE*)base, info.SizeOfImage);
 
 	return 0;
 }
-
 __declspec(noinline) void hookDemoMode(){
 	UINT64 demoModeAddr = aobScanProcess(AOB, sizeof(AOB), BitMasks); //searching for the demoMode
 	
@@ -167,7 +101,6 @@ __declspec(noinline) void hookDemoMode(){
 		}
 	}
 }
-
 BOOL WINAPI DllMain(HINSTANCE hInst,DWORD reason,LPVOID){
 	if (reason == DLL_PROCESS_ATTACH){
 		char path[1000];
